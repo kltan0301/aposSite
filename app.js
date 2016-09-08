22790e6320a360eb6787a60db8efaf1bfcc4fed4
Kai Lin Tan <kailin.tan.2010@gmail.com>
2016-09-08 16:36:56 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var apos = require('apostrophe')({
+  shortName: 'apostrophesandbox',
+  title: 'Apostrophe Sandbox 2.0.0',
+  demo: true,
+  
+  bundles: ['apostrophe-blog'],
+
+  // These are the modules we want to bring into the project.
+  modules: {
+    
+    'apostrophe-templates': { viewsFolderFallback: __dirname + '/views' },
+    'apostrophe-express': {
+      session: {
+        secret: 'ksajhfkdsfha43fahif3a8asdfkyfsd7f'
+      }
+    },
+    
+    // Standard Apostrophe Modules
+    'apostrophe-assets': {},
+    'apostrophe-blog': {},
+    'apostrophe-blog-pages': {},
+    'apostrophe-blog-widgets': {},
+    'apostrophe-users': {},
+    
+    // Apostrophe Sandbox (as-) specific modules
+    'as-helpers': {},
+    'as-two-column-block-widgets': {}
+  }
+
+});
